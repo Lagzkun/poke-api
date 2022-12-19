98369e283ab2c8a6b97d6c9baf5e4d502e982775
Lagzkun <lagzlight@.com>
2022-12-19 19:46:25 -0300

    botones de siguiente y anterior

--- src/components/Pokemons.jsx
+++ src/components/Pokemons.jsx
@@ -3,23 +3,30 @@ import React, { useState, useEffect } from 'react'
 function Pokemons() {
 
     const [pokemons, setPokemons] = useState([]);
+    const [before, setBefore] = useState(null);
+    const [after, setAfter] = useState(null);
+    const [current, setCurrent] = useState('https://pokeapi.co/api/v2/pokemon/?limit=150&offset=0')
 
     useEffect(() => {
         async function getPokemons() {
-            const response = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=150&offset=0');
+            const response = await fetch(current);
             const data = await response.json();
-
+            console.log(data);
             setPokemons(data.results);
+            setBefore(data.previous);
+            setAfter(data.next);
         }
 
         getPokemons();
-    },[])
+    },[current])
 
   return (
     <div>
       {pokemons.map((pokemon, index) => {
         return <li key={index}>{pokemon.name}</li>
       })}
+      <button onClick={() => before !== null && setCurrent(before)}>Anterior</button>
+      <button onClick={() => after !== null && setCurrent(after)}>Siguiente</button>
     </div>
   )
 }
